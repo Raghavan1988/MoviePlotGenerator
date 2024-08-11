78de19484ce97eed126a7dda52807f8a5bf88139
Raghavan <raghavan.mit@gmail.com>
2024-08-11 14:07:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -78,5 +78,19 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# Add custom CSS to hide the GitHub icon
+st.markdown(
+    """
+    <style>
+    /* Hide the GitHub icon */
+    .viewerBadge_container__1QSob { 
+        display: none; 
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
 if __name__ == '__main__':
     main()
